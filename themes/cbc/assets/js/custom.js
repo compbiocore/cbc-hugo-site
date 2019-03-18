3ada4d5c4194a3005585a44d6bd4a641836a82f3
fernandogelin <fernandogelin@gmail.com>
2019-03-18 11:48:39 -0400

    fix(navbar):

---
+++ themes/cbc/assets/js/custom.js
@@ -0,0 +1,11 @@
+$("document").ready(function($){
+    var nav = $('.navbar');
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            nav.addClass("f-nav");
+        } else {
+            nav.removeClass("f-nav");
+        }
+    });
+});
